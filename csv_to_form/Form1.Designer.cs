03a72a0884535e50f878c11f6ee508fdb0e2773f
em-yabloko <57377898+em-yabloko@users.noreply.github.com>
2023-04-16 23:19:24 +0700

    Add files via upload

---
+++ csv_to_form/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace csv_to_form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Code = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Name1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Area = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(33, 716);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(390, 100);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Загрузить таблицу";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.Code,
+            this.Name1,
+            this.Area});
+            this.dataGridView1.Location = new System.Drawing.Point(33, 108);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1200, 585);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // id
+            // 
+            this.id.HeaderText = "ID";
+            this.id.MinimumWidth = 8;
+            this.id.Name = "id";
+            this.id.Width = 70;
+            // 
+            // Code
+            // 
+            this.Code.HeaderText = "ФИО";
+            this.Code.MinimumWidth = 200;
+            this.Code.Name = "Code";
+            this.Code.Width = 600;
+            // 
+            // Name1
+            // 
+            this.Name1.HeaderText = "Время вылета";
+            this.Name1.MinimumWidth = 100;
+            this.Name1.Name = "Name1";
+            this.Name1.Width = 250;
+            // 
+            // Area
+            // 
+            this.Area.HeaderText = "№ рейса";
+            this.Area.MinimumWidth = 100;
+            this.Area.Name = "Area";
+            this.Area.Width = 215;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(97, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(414, 40);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Рейсы авиапассажиров";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(445, 716);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(390, 100);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Сохранить таблицу";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1278, 844);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Рейсы авиапассажиров";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Code;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Area;
+    }
+}
+
---
+++ csv_to_form/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic.FileIO;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using Microsoft.Office.Interop.Excel;
+
+namespace csv_to_form
+{
+        public partial class Form1 : Form
+    {
+        // txt посредник
+        string addr2 = @"C:\temp_csv\test.txt";
+        string addr3 = @"C:\temp_csv\Book1.csv";
+
+        public Form1()
+        {
+            InitializeComponent();                        
+        }
+
+        private SqlConnection conn = null;
+        private string source_csv;
+        public string open_file(string dialogue) // диалог выбора файла
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = dialogue;
+            ofd.InitialDirectory = @"c:\";
+            ofd.Filter = "All files (*.*)|*.csv|All files (*.*)|*.*";
+            ofd.FilterIndex = 1;
+            ofd.RestoreDirectory = true;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                source_csv = ofd.FileName;
+            }
+            return source_csv;
+        }
+
+        // кнопка для открытия сохраненной таблицы
+        private void button1_Click(object sender, EventArgs e)
+        {   
+            string new_addr1 = open_file("Выберите, из какого файла загрузить таблицу (формат CSV)"); // адрес открытого
+            if (new_addr1 != null)          // если адрес не отменен
+            {
+                // перекодировка в кириллицу
+                using (StreamReader sr = new StreamReader(new_addr1, Encoding.Default))
+                {
+                    // сброс в нужной кодировке посреднику
+                    using (FileStream fs = File.Create(addr2))
+                    {
+                        while (sr.EndOfStream == false)
+                        {
+                            string data = sr.ReadLine();
+                            byte[] info = new UTF8Encoding(true).GetBytes(data + Environment.NewLine);
+                            fs.Write(info, 0, info.Length);
+                        }
+                    }
+                }
+                cll();  // разбивка на кадры из txt и склад в sql
+
+                // из sql в таблицу Form1
+                string query = "SELECT * FROM [Table] ORDER BY Id";
+                SqlCommand cmd_grid = new SqlCommand(query, conn);
+                SqlDataReader dr = cmd_grid.ExecuteReader();
+                List<string[]> list = new List<string[]>();
+                while (dr.Read())
+                {
+                    list.Add(new string[4]);
+
+                    list[list.Count - 1][0] = dr[0].ToString();
+                    list[list.Count - 1][1] = dr[1].ToString();
+                    list[list.Count - 1][2] = dr[2].ToString();
+                    list[list.Count - 1][3] = dr[3].ToString();
+                }
+                dr.Close();
+
+                dataGridView1.Rows.Clear();
+                foreach (string[] list2 in list)
+                    dataGridView1.Rows.Add(list2);
+            }
+        }
+
+        // разбивка на кадры из txt и склад в sql
+        void cll()
+        {
+            using (TextFieldParser res = new TextFieldParser(addr2))
+            {
+                SqlCommand cmd_clr = new SqlCommand($"TRUNCATE TABLE [Table]", conn);
+                cmd_clr.ExecuteNonQuery();
+
+                res.TextFieldType = FieldType.Delimited;
+                res.SetDelimiters(";");
+
+                while (!res.EndOfData)
+                {
+                    string[] datas = res.ReadFields();
+
+                    SqlCommand cmd_add = new SqlCommand($"INSERT INTO [Table] (Code, Name, Area) VALUES (N'{datas[0]}',N'{datas[1]}',N'{datas[2]}')", conn);
+                    cmd_add.ExecuteNonQuery();
+                }
+            }
+        }
+
+         private void Form1_Load(object sender, EventArgs e)
+        {
+            // коннект к sql
+            string way = System.Windows.Forms.Application.StartupPath; // текущая директория
+            string addr_conn = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = "+way+@"Database1.mdf;Integrated Security=True";
+            conn = new SqlConnection(addr_conn);
+
+            conn.Open();
+
+            // папка для посредника
+            DirectoryInfo di = new DirectoryInfo(@"c:\temp_csv");
+            if (!di.Exists)
+            {
+                di.Create();
+            }
+
+            if (!File.Exists(addr3))
+            using (FileStream fs = File.Create(addr3))
+            {
+
+            }
+        }
+
+        // кнопка сохранения
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Saver();
+        }
+
+        // сохраняем из dataGrid в Excel
+        public void Saver()
+        {
+            Microsoft.Office.Interop.Excel.Application ExcelApp = new Microsoft.Office.Interop.Excel.Application();
+            Microsoft.Office.Interop.Excel.Workbook ExcelWorkBook;
+            Microsoft.Office.Interop.Excel.Worksheet ExcelWorkSheet;
+
+            //Книга
+            string excl_file = open_file("Выберите файл, в который хотите сохранить таблицу (формат CSV)");
+
+            if (excl_file != null)
+            {
+                ExcelWorkBook = ExcelApp.Workbooks.Open(excl_file);
+
+                //Таблица
+                ExcelWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)ExcelWorkBook.Worksheets.get_Item(1);
+                ExcelWorkSheet.Cells.ClearContents();
+
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                    {
+                        ExcelApp.Cells[i + 1, j + 1].EntireColumn.NumberFormat = "@";  // все как текст
+                        ExcelApp.Cells[i + 1, j + 1] = dataGridView1.Rows[i].Cells[j].Value;
+                    }
+                }
+                //Вызываем excel файл
+                ExcelApp.Visible = true;
+                ExcelApp.UserControl = true;
+            }
+        }
+    }
+}
